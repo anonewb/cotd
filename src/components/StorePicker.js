38da371f0e329cbcc53940bdea9e16b9c95aa328
anonewb <anonewbe@gmail.com>
2018-07-05 19:36:18 +0530

    ***events, refs and binding in react

--- src/components/StorePicker.js
+++ src/components/StorePicker.js
@@ -2,11 +2,26 @@ import React from 'react';
 import { getFunName } from '../helpers';
 
 class StorePicker extends React.Component {
+
+  myInput = React.createRef();
+
+  // binding in react
+  // as we used "this" in pur custom fn below, to bind the method we converted to arrow fn which becomes property to component object thus value of "this" now point to StorePicker obj
+
+  goToStore = event => {
+    // 1. stop the form from submitting
+    event.preventDefault();
+    // 2. get the text from input
+    console.log(this);
+    // 3. change the pg to /store/whatever-they-entered
+
+  }
+
   render() {
     return (
-      <form className="store-selector">
+      <form className="store-selector" onSubmit={this.goToStore}>
         <h2>Please Enter a Store</h2>
-        <input type="text" required placeholder="Store Name" defaultValue={getFunName()} />
+        <input type="text" ref={this.myInput} required placeholder="Store Name" defaultValue={getFunName()} />
         <button type="submit">Visit Store -></button>
       </form>
     )
