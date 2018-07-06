562e17c0019a23b5cb650e1106a1bb1db4d886cb
anonewb <anonewbe@gmail.com>
2018-07-06 12:50:02 +0530

    ***understand state; create fish state

--- src/components/App.js
+++ src/components/App.js
@@ -4,6 +4,24 @@ import Order from './Order';
 import Inventory from './Inventory';
 
 class App extends React.Component {
+
+  // created below state here in App compo coz 
+  // 1. we want this state to be used in our entire app and
+  // 2. data(like addFish property) can be passed to lower compo while lower to upper is not possible.
+  state = {
+    fishes: {},
+    order: {}
+  };
+
+  addFish = fish => {
+    // 1. take a copy of the existing state
+    const fishes = { ...this.state.fishes };
+    // 2. add our new fish to that fishes variable
+    fishes[`fish${Date.now()}`] = fish;
+    // 3. set the new fishes object to state
+    this.setState({ fishes: fishes });
+  };
+
   render() {
     return (
       <div className="catch-of-the-day">
@@ -11,7 +29,7 @@ class App extends React.Component {
           <Header tagline="Fresh Seafood Market" />
         </div>
         <Order />
-        <Inventory />
+        <Inventory addFish={this.addFish} /> {/* "addFish" is stored in props and passed to further downward compo (like Inventory) where it can be accessed */}
       </div>
     );
   }
--- src/components/Inventory.js
+++ src/components/Inventory.js
@@ -1,9 +1,13 @@
 import React from 'react';
+import AddFishForm from './AddFishForm';
 
 class Inventory extends React.Component {
   render() {
     return (
-      <div className="inventory">Inventory!!</div>
+      <div className="inventory">
+        <h2>Inventory!!</h2>
+        <AddFishForm addFish={this.props.addFish}/> {/* "addFish" is stored in props and passed to further downward compo (like AddFishForm) where it can be accessed */}
+      </div>
     );
   }
 }
